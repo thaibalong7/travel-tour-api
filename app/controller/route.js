9bf906db30c13a6004042d7b43cbc49f6b830916
thaibalong7 <thai.balong.7@gmail.com>
2019-02-28 19:34:42 +0700

    ! change api getRouteByTour

--- app/controller/route.js
+++ app/controller/route.js
@@ -14,6 +14,30 @@ addLinkFeaturedImg = async (_routes, host) => {
     })
 }
 
+addLinkFeaturedImgAndTour = async (_routes, host) => {
+    return _routes.map(async item => {
+        const query = {
+            attributes: ['id', 'name'],
+            include: [
+                {
+                    attributes: [],
+                    model: db.routes,
+                    where: {
+                        fk_location: item.fk_location
+                    }
+                }]
+        }
+        item.location.dataValues.tours = await db.tours.findAll(query);
+        if (item.featured_img === null) {
+            // location.featured_img = host + '/assets/images/locationDefault/' + item.fk_type + '.jpg';
+        }
+        else {
+            item.featured_img = host + '/assets/images/locationFeatured/' + item.featured_img;
+        }
+        return item;
+    })
+}
+
 exports.create = (req, res) => {
     routes.create(req.body).then(_tour => {
         res.status(200).json(_tour)
@@ -35,17 +59,12 @@ exports.getByTour = (req, res) => {
         order: [['day', 'ASC'], ['arrive_time', 'ASC']]
     }
     routes.findAll(query).then(async _routes => {
-        const result = await addLinkFeaturedImg(_routes, req.headers.host)
-        const tour = await db.tours.findAll({
-            attributes: ['id', 'name'],
-            where: {
-                id: idTour
-            }
-        })
-        res.status(200).json({
-            data: result,
-            tour: tour
-        })
+        const result = await addLinkFeaturedImgAndTour(_routes, req.headers.host)
+        Promise.all(result).then(completed =>{
+            res.status(200).json({
+                data: completed,
+            })
+        })  
     }).catch(err => {
         res.status(400).json({ msg: err })
     })
