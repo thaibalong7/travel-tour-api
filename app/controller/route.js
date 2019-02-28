732f137ffcfd02d2643e358300e13b5ea1c66cf8
thaibalong7 <thai.balong.7@gmail.com>
2019-02-28 17:55:15 +0700

    ! change API getRouteByTour

--- app/controller/route.js
+++ app/controller/route.js
@@ -1,6 +1,19 @@
 const db = require('../models');
 const routes = db.routes;
 
+addLinkFeaturedImg = async (_routes, host) => {
+    return _routes.map(item => {
+        if (item.location.featured_img === null) {
+            // item.featured_img = host + '/assets/images/locationDefault/' + item.fk_type + '.jpg';
+            return item;
+        }
+        else {
+            item.location.featured_img = host + '/assets/images/locationFeatured/' + item.location.featured_img;
+            return item;
+        }
+    })
+}
+
 exports.create = (req, res) => {
     routes.create(req.body).then(_tour => {
         res.status(200).json(_tour)
@@ -21,8 +34,18 @@ exports.getByTour = (req, res) => {
         }],
         order: [['day', 'ASC'], ['arrive_time', 'ASC']]
     }
-    routes.findAll(query).then(_routes => {
-        res.status(200).json({ data: _routes })
+    routes.findAll(query).then(async _routes => {
+        const result = await addLinkFeaturedImg(_routes, req.headers.host)
+        const tour = await db.tours.findAll({
+            attributes: ['id', 'name'],
+            where: {
+                id: idTour
+            }
+        })
+        res.status(200).json({
+            data: result,
+            tour: tour
+        })
     }).catch(err => {
         res.status(400).json({ msg: err })
     })
