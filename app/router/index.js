2e4a765665dcb71eba7b801f9ffa8f2b79c197ba
thaibalong7 <thai.balong.7@gmail.com>
2019-03-07 20:40:49 +0700

    + add api getNumOfTourAndLocation

--- app/router/index.js
+++ app/router/index.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-const middlewareAuth = require('../middleware/auth')
+const db = require('../models');
 
 router.use('/admin', require('./admin'));
 router.use('/user', require('./user'));
@@ -9,5 +9,14 @@ router.use('/tour', require('./tour'));
 router.use('/route', require('./route'))
 router.use('/tour_turn', require('./tour_turn'));
 router.use('/comment', require('./comment'));
+
+router.get('/getNumOfTourAndLocation', async (req, res) => {
+    const all_tours = await db.tours.findAll();
+    const all_locations = await db.locations.findAll();
+    res.status(200).json({
+        num_of_tours: all_tours.length,
+        num_of_locations: all_locations.length
+    })
+})
 
 module.exports = router
