a48e88576c1dd54f9bf30d3256f3e2a611b918c0
thaibalong7 <thai.balong.7@gmail.com>
2019-03-09 21:13:48 +0700

    ! fix validate list routes

--- app/helper/validate.js
+++ app/helper/validate.js
@@ -3,21 +3,21 @@ const check_time = async (arrive, leave) => {
     return Date.parse('01/01/2011 ' + arrive) < Date.parse('01/01/2011 ' + leave)
 }
 
-const check_2_routes = async (i, routes1, routes2) => {
+const check_2_routes = async (i, routes1, routes2, length) => {
     if (typeof routes1.id === undefined || typeof routes1.day === undefined
         || typeof routes2.id === undefined || typeof routes2.day === undefined
         || typeof routes1.arriveTime === undefined || typeof routes1.leaveTime === undefined
         || typeof routes2.arriveTime === undefined || typeof routes2.leaveTime === undefined
         || isNaN(routes1.id) || isNaN(routes2.id)
-        || isNaN(routes1.day) || isNaN(routes2.day)) {
+        || isNaN(routes1.day) || isNaN(routes2.day)) { //check dữ liệu truyền vào có đúng
         return false;
     }
     else {
-        if (i === 1 && parseInt(routes1.day) !== 1) {
+        if (i === 0 && ((parseInt(routes1.day) !== 1) || routes1.leaveTime === null)) { //routes đầu tiên và có day khác 1 hoặc leaveTime là null
             return false;
         }
         else {
-            if (i !== 1 && (routes1.arriveTime === null || routes1.leaveTime === null)) //k phải route đầu tiên và có thời gian là null
+            if (i !== 0 && (routes1.arriveTime === null || routes1.leaveTime === null)) //k phải route đầu tiên và có thời gian là null
             {
                 return false;
             }
@@ -38,10 +38,20 @@ const check_2_routes = async (i, routes1, routes2) => {
                         }
                     }
                     else {
-                        if (parseInt(routes1.day) > parseInt(routes2.day)) {
+                        if (parseInt(routes1.day) > parseInt(routes2.day)) { //routes trước có day lớn hơn routes sau
                             return false;
                         } else {
-                            return true;
+                            if (parseInt(routes1.id) === parseInt(routes2.id)) { //hai routes kề nhau có cùng điểm đến
+                                return false
+                            }
+                            else {
+                                if (i == length - 2 && routes2.leaveTime === null) { //routes2 là route cuối, có leaveTime là null
+                                    return false;
+                                }
+                                else {
+                                    return true;
+                                }
+                            }
                         }
                     }
                 }
@@ -52,7 +62,7 @@ const check_2_routes = async (i, routes1, routes2) => {
 
 const asyncFor = async (routes, cb) => {
     for (var i = 0; i < routes.length - 1; i++) {
-        if (!(await check_2_routes(i, routes[i], routes[i + 1]))) {
+        if (!(await check_2_routes(i, routes[i], routes[i + 1], routes.length))) {
             cb(false);
             break;
         };
