31a5e6ddcda0d0a1f87e72ca2f15879376ce7a54
thaibalong7 <thai.balong.7@gmail.com>
2019-05-10 15:26:15 +0700

    ! fix check policy allow booking

--- app/helper/check_policy.js
+++ app/helper/check_policy.js
@@ -7,7 +7,7 @@ const check_policy_allow_booking = (tour_turn) => {
     const cur_date = new Date();
     const timeDiff = new Date(tour_turn.start_date) - cur_date;
     const days_before_go = parseInt(timeDiff / (1000 * 60 * 60 * 24) + 1) //số ngày còn lại trc khi đi;
-    if (days_before_go > 3) return true;
+    if (days_before_go > parseInt(tour_turn.booking_term)) return true;
     else return false;
 }
 
@@ -20,8 +20,35 @@ const add_is_allow_booking = async (tour_turns, isRawObj = true) => {
     }
 }
 
+const check_policy_cancel_booking = async (booking) => {
+    //check theo tour turn của booking này
+    if (booking.status == 'paid') {
+        const start_date = new Date(booking.tour_turn.start_date);
+        const cur_date = new Date();
+        if (booking.payment_method.name == 'online')
+            if (cur_date < start_date)
+                return true;
+            else return false;
+        else { // payment_method là incash và tranfer
+            const timeDiff = (start_date) - (cur_date);
+            const days = timeDiff / (1000 * 60 * 60 * 24) //số ngày còn lại trc khi đi
+            if (days > 6) //nếu còn lại trên 7 ngày -> cho phép request hủy
+                return true;
+            else return false //ngược lại tới cty mà chuyển
+        }
+    }
+    return false; // book tour chưa thanh toán hoặc đang chờ hủy hoặc đã bị hủy hoặc đã đi thì k thể cancel
+}
+
+const add_is_cancel_booking = async (list_booking) => { //thêm 1 record là dựa vào policy thì hiện giờ có thể hủy đặt tour hay k
+    for (var i = 0; i < list_booking.length; i++) {
+        list_booking[i].isCancelBooking = await check_policy_cancel_booking(list_booking[i]);
+    }
+}
 
 module.exports = {
     check_policy_allow_booking,
-    add_is_allow_booking
+    add_is_allow_booking,
+    check_policy_cancel_booking,
+    add_is_cancel_booking
 }
