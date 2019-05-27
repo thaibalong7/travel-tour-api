56c1e4839b62ab9019f1847082268d2832f73106
thaibalong7 <thai.balong.7@gmail.com>
2019-05-27 23:22:09 +0700

    ! update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var env = require('dotenv');
 var cronjob = require('./app/cronjob');
 var logger = require('morgan');
 var FroalaEditor = require('wysiwyg-editor-node-sdk/lib/froalaEditor.js');
+// var socket = require('./app/socket');
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const configFileMapping = {
     'development': './env/.env.development',
@@ -26,7 +28,7 @@ env.config({ path: configFileMapping[process.env.NODE_ENV] })
 //     optionsSuccessStatus: 204
 //   }));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === 'production' ? "*" : "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === 'production' ? "*" : "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
@@ -64,13 +66,17 @@ models.sequelize.sync().then(function () {
 });
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-var server = app.listen(server_port, server_host, function (err) {
+var server_host = process.env.YOUR_HOST || '127.0.0.1';
+var server = (app).listen(server_port, function (err) {
     if (!err)
-        console.log(`App listening at port ${server_port}`);
+        console.log(`App listening at  http://${server_host}:${server_port}`);
     else console.log(err)
 });
 
+// var io = require('socket.io').listen(server);
+// console.log(io)
+
+// socket.createSocketServer();
 cronjob.cron_ckeck_token();
 
 
